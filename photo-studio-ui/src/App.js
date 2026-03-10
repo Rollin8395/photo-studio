5ea34ca34e079c5edaf1a6a0bb42af66ddb6bfb0
Beerus <rollinyambemrkmv91@gmail.com>
2026-03-10 17:27:06 +0100

    added some more functions and inital stage of frontend

---
+++ photo-studio-ui/src/App.js
@@ -0,0 +1,23 @@
+import UploadPage from "./pages/UploadPage";
+import OrdersPage from "./pages/OrdersPage";
+
+function App() {
+
+  return (
+
+    <div>
+
+      <h1>Photo Studio System</h1>
+
+      <UploadPage />
+
+      <hr />
+
+      <OrdersPage />
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ photo-studio-ui/src/pages/UploadPage.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import API from "../api/api";
+
+function UploadPage() {
+
+  const [file, setFile] = useState(null);
+
+  const uploadPhoto = async () => {
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    await API.post("/uploads", formData);
+
+    alert("Photo uploaded!");
+  };
+
+  return (
+    <div>
+
+      <h2>Upload Photo</h2>
+
+      <input
+        type="file"
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+
+      <button onClick={uploadPhoto}>
+        Upload
+      </button>
+
+    </div>
+  );
+}
+
+export default UploadPage;
